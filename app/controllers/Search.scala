8cf7a45c0d92583da7b76565b8a863a47a991916
Cornic Mathieu <mathieu.cornic@soprasteria.com>
2016-11-04 17:18:49 +0100

    fix #4 : response body is displayed even when application/json or xml is used with charset/url

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -5,13 +5,17 @@ import play.api.libs.json._
 import models._
 import models.UtilDate._
 import play.api.libs.iteratee.Enumerator
-import play.api.http.{HeaderNames}
+import play.api.http.HeaderNames
+
 import scala.xml.PrettyPrinter
 import org.xml.sax.SAXParseException
 import java.net.URLDecoder
-import scala.concurrent.{Await, Future, ExecutionContext}
+import java.util.regex.Pattern
+
+import scala.concurrent.{Await, ExecutionContext, Future}
 import ExecutionContext.Implicits.global
-import reactivemongo.bson.{BSONObjectID, BSONString, BSONDocumentWriter, BSONDocument}
+import reactivemongo.bson.{BSONDocument, BSONDocumentWriter, BSONObjectID, BSONString}
+
 import scala.util.parsing.json.JSONObject
 import scala.concurrent.duration._
 import com.fasterxml.jackson.core.JsonParseException
@@ -151,36 +155,39 @@ object Search extends Controller {
    */
   def getResponse(id: String) = Action.async {
     RequestData.loadResponse(id).map {
-      tuple => tuple match {
-        case Some(doc: BSONDocument) => {
-          doc.getAs[String]("contentType").get match {
-            case "application/json" =>
-              var content = doc.getAs[String]("response").get
-              try {
-                val content = Json.parse(doc.getAs[String]("response").get)
-                Ok(Json.toJson(content));
-              }
-              catch {
-                case e: JsonParseException =>
-                  Ok(content)
-              }
-            case "application/xml" | "text/xml" => {
-              var content = doc.getAs[String]("response").get
-              try {
-                content = new PrettyPrinter(250, 4).format(scala.xml.XML.loadString(content))
-              } catch {
-                case e: SAXParseException =>
+      case Some(doc: BSONDocument) => {
 
-              }
-              Ok(content)
+        val patternJson = ".*(application/json).*".r
+        val patternXml = ".*(application/xml).*".r
+        val patternTextXml = ".*(text/xml).*".r
+
+        doc.getAs[String]("contentType").get match {
+          case patternJson(_) =>
+            val content = doc.getAs[String]("response").get
+            try {
+              val content = Json.parse(doc.getAs[String]("response").get)
+              Ok(Json.toJson(content));
             }
-            case _ =>
-              Ok(doc.getAs[String]("response").get)
+            catch {
+              case e: JsonParseException =>
+                Ok(content)
+            }
+          case patternXml(_) | patternTextXml(_) => {
+            var content = doc.getAs[String]("response").get
+            try {
+              content = new PrettyPrinter(250, 4).format(scala.xml.XML.loadString(content))
+            } catch {
+              case e: SAXParseException =>
+
+            }
+            Ok(content)
           }
+          case _ =>
+            Ok(doc.getAs[String]("response").get)
         }
-        case None =>
-          NotFound("The response does not exist")
       }
+      case None =>
+        NotFound("The response does not exist")
     }
   }
 
@@ -191,7 +198,7 @@ object Search extends Controller {
    */
   def downloadResponse(id: String) = Action.async {
     val future = RequestData.loadResponse(id)
-    downloadInCorrectFormat(future, id, false)
+    downloadInCorrectFormat(future, id, isRequest = false)
   }
 
 
@@ -216,61 +223,62 @@ object Search extends Controller {
     var contentInCorrectFormat = ""
 
     future.map {
-      tuple => tuple match {
-        case Some(doc: BSONDocument) => {
-          val contentType = doc.getAs[String]("contentType").get
-          // doc.getAs[String]("response")
-          val content = doc.getAs[String](keyContent).get
+      case Some(doc: BSONDocument) => {
+        val contentType = doc.getAs[String]("contentType").get
+        // doc.getAs[String]("response")
+        val content = doc.getAs[String](keyContent).get
+        val patternJson = ".*(application/json).*".r
+        val patternXml = ".*(application/xml).*".r
+        val patternTextXml = ".*(text/xml).*".r
 
-          contentType match {
-            case "application/xml" | "text/xml" => {
-              try {
-                contentInCorrectFormat = new PrettyPrinter(250, 4).format(scala.xml.XML.loadString(content))
-                filename += ".xml"
-              } catch {
-                case e: SAXParseException => contentInCorrectFormat = content
-                  filename += ".txt"
-              }
-
-              var result = Result(
-                header = ResponseHeader(play.api.http.Status.OK),
-                body = Enumerator(contentInCorrectFormat.getBytes))
-
-                result = result.withHeaders((HeaderNames.CONTENT_DISPOSITION, "attachment; filename=" + filename))
-                result.as(XML)
-
+        contentType match {
+          case patternXml(_) | patternTextXml(_) => {
+            try {
+              contentInCorrectFormat = new PrettyPrinter(250, 4).format(scala.xml.XML.loadString(content))
+              filename += ".xml"
+            } catch {
+              case e: SAXParseException => contentInCorrectFormat = content
+                filename += ".txt"
             }
 
-            case "application/json" => {
-              try {
-                contentInCorrectFormat = Json.parse(content).toString
-                filename += ".json"
-              }
-              catch {
-                case e: Exception =>
-                  contentInCorrectFormat = content
-                  filename += ".txt"
-              }
-              var result = Result(
-                header = ResponseHeader(play.api.http.Status.OK),
-                body = Enumerator(contentInCorrectFormat.getBytes))
-                result = result.withHeaders((HeaderNames.CONTENT_DISPOSITION, "attachment; filename=" + filename))
-                result.as(JSON)
-            }
-            case _ => {
-              filename += ".txt"
-              var result = Result(
-                header = ResponseHeader(play.api.http.Status.OK),
-                body = Enumerator(content.getBytes))
-                result = result.withHeaders((HeaderNames.CONTENT_DISPOSITION, "attachment; filename=" + filename))
-                result.as(TEXT)
-            }
+            var result = Result(
+              header = ResponseHeader(play.api.http.Status.OK),
+              body = Enumerator(contentInCorrectFormat.getBytes))
+
+            result = result.withHeaders((HeaderNames.CONTENT_DISPOSITION, "attachment; filename=" + filename))
+            result.as(XML)
 
           }
+
+          case patternJson(_) => {
+            try {
+              contentInCorrectFormat = Json.parse(content).toString
+              filename += ".json"
+            }
+            catch {
+              case e: Exception =>
+                contentInCorrectFormat = content
+                filename += ".txt"
+            }
+            var result = Result(
+              header = ResponseHeader(play.api.http.Status.OK),
+              body = Enumerator(contentInCorrectFormat.getBytes))
+            result = result.withHeaders((HeaderNames.CONTENT_DISPOSITION, "attachment; filename=" + filename))
+            result.as(JSON)
+          }
+          case _ => {
+            filename += ".txt"
+            var result = Result(
+              header = ResponseHeader(play.api.http.Status.OK),
+              body = Enumerator(content.getBytes))
+            result = result.withHeaders((HeaderNames.CONTENT_DISPOSITION, "attachment; filename=" + filename))
+            result.as(TEXT)
+          }
+
         }
-        case _ =>
-          NotFound("The request does not exist")
       }
+      case _ =>
+        NotFound("The request does not exist")
     }
 
   }
